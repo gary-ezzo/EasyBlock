586dadde5603fe5c01611e62063f7772c3933894
gary-ezzo <gary.p.ezzo@gmail.com>
2026-01-13 21:36:38 -0800

    Initial Commit

---
+++ EasyBlock/EasyBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBlockApp.swift
+//  EasyBlock
+//
+//  Created by gary ezzo on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct EasyBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
